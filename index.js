6868c2d789eddeedda58002ce99df136135e9f31
Vishnuteja <vishnuteja476@gamil.com>
2023-03-28 09:55:28 +0530

    weather app works fine

---
+++ index.js
@@ -0,0 +1,40 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7fd3d9ded6msh22beb0679283fd4p1228e8jsn10a5af4dde67",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWeather = (city) => {
+  cityname.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Delhi");
